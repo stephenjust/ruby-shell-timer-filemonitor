0b679d0b5fac7c2ce7351df21bfc07bec6c683ed
jesseDtucker <jesseDtucker@gmail.com>
2014-10-16 13:23:19 -0600

    Added cd to shell contract

--- lib/contracts/shell_contract.rb
+++ lib/contracts/shell_contract.rb
@@ -17,12 +17,20 @@ module Contracts
       assert self.is_valid_cmd(cmdInput?)
     end
     
+    def pre_cd(target)
+      assert_not_nil target
+      assert File.directory?(target), "#{target} is not a directory!"
+    end
+    
     def class_invariant
       assert self.responds_to?(:execute_cmd), "Shell must support execute_cmd"
       assert self.responds_to?(:is_internal_cmd?), "Shell must support is_internal_cmd?"
       assert self.responds_to?(:run) , "Shell must support run"
       assert self.responds_to?(:is_valid_cmd?), "Shell must support is_valid_cmd?"
+      assert self.responds_to?(:cd), "FileWatcher must support cd"
+      assert self.responds_to?(:pwd), "FileWatcher must support pwd"
       assert(File.directory?(self.pwd), "Current path #{self.pwd} must be a directory.")
+      
     end
 
   end
